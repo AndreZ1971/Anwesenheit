1c4e573d381813093d5a830617714d0efc0c5248
Andre Zabel <andre.zabel71@gmail.com>
2024-10-07 10:25:38 +0200

    Add files via upload

---
+++ schueler_[deine_nummer].py
@@ -0,0 +1,14 @@
+def schuelerdaten():
+  """
+  Gibt die Daten des Schülers zurück.
+  """
+  name = "[Dein Name]"  # Hier deinen Namen eintragen
+  alter = [Dein Alter]  # Hier dein Alter eintragen
+  # ... weitere Daten hinzufügen
+
+  return f"Name: {name}, Alter: {alter}"  # ... weitere Daten in der Ausgabe
+
+# (Optional) Test, ob das Skript direkt ausgeführt wird
+if __name__ == "__main__":
+  print(schuelerdaten())
+  
